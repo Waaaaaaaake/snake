3d2028124c11d01d81e8c7baf8f877ac00966e89
Stas <lukyanov.stas@mail.ru>
2019-08-28 15:32:38 +0200

    2

---
+++ Snake1/Snake1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char symbol = '*';
+
+            Console.SetCursorPosition(x1, y1); ;
+            Console.Write(symbol);
+            Console.ReadLine();
+
+        }
+    }
+}
